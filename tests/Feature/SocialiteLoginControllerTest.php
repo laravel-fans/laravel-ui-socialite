1184b6f87c18c31e15a1419f374e9c9b57597972
sinkcup <sinkcup@gmail.com>
2019-09-07 11:45:00 +0800

    opt: increase test coverage. #20

--- tests/Feature/SocialiteLoginControllerTest.php
+++ tests/Feature/SocialiteLoginControllerTest.php
@@ -3,11 +3,14 @@
 namespace sinkcup\LaravelMakeAuthSocialite\Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Socialite\Facades\Socialite;
+use Mockery;
+use sinkcup\LaravelMakeAuthSocialite\SocialAccount;
 use sinkcup\LaravelMakeAuthSocialite\Tests\TestCase;
 
 class SocialiteLoginControllerTest extends TestCase
 {
-    use RefreshDatabase;
+    //use RefreshDatabase;
 
     public function testLogin()
     {
@@ -16,5 +19,60 @@ class SocialiteLoginControllerTest extends TestCase
         $response->assertViewIs('auth.login');
         $response->assertViewHas('social_login', config('auth.social_login'));
         $response->assertViewHas('password_login', config('auth.password_login'));
+    }
+
+    public function testRedirectToProvider()
+    {
+        $provider = 'github';
+        $this->app['config']->set('services.' . $provider, $this->serviceConfig);
+
+        $response = $this->get('/login/' . $provider);
+        $authUrl = 'https://github.com/login/oauth/authorize?' . http_build_query([
+            'client_id' => $this->serviceConfig['client_id'],
+            'redirect_uri' => $this->serviceConfig['redirect'],
+            'scope' => 'user:email',
+            'response_type' => 'code',
+            'state' => session('state'),
+        ]);
+
+        $response->assertRedirect($authUrl);
+    }
+
+    public function testHandleProviderCallback()
+    {
+        $provider = 'github';
+        $this->app['config']->set('services.' . $provider, $this->serviceConfig);
+        $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
+        $abstractUser->token = $this->faker->md5;
+        $socialAccount = [
+            'provider_user_id' => $this->faker->randomNumber(),
+            'name' => $this->faker->userName,
+            'nickname' => $this->faker->name,
+            'email' => $this->faker->email,
+            'avatar' => $this->faker->url,
+            'raw' => [],
+        ];
+        $abstractUser
+            ->shouldReceive('getId')
+            ->andReturn($socialAccount['provider_user_id'])
+            ->shouldReceive('getName')
+            ->andReturn($socialAccount['name'])
+            ->shouldReceive('getNickname')
+            ->andReturn($socialAccount['nickname'])
+            ->shouldReceive('getEmail')
+            ->andReturn($socialAccount['email'])
+            ->shouldReceive('getAvatar')
+            ->andReturn($socialAccount['avatar'])
+            ->shouldReceive('getRaw')
+            ->andReturn($socialAccount['raw']);
+        Socialite::shouldReceive('driver->scopes->user')->andReturn($abstractUser);
+
+        $response = $this->get('/login/' . $provider . '/callback');
+        $response->assertRedirect(route('profile.edit'));
+        $this->assertEquals(1, SocialAccount::count());
+        $socialAccountDb = SocialAccount::first();
+        foreach ($socialAccount as $k => $v) {
+            $this->assertEquals($v, $socialAccountDb->{$k});
+        }
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,16 @@
 
 namespace sinkcup\LaravelMakeAuthSocialite\Tests;
 
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Route;
 use sinkcup\LaravelMakeAuthSocialite\PackageServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
 {
+    use WithFaker;
+    protected $serviceConfig;
+
     protected function getPackageProviders($app)
     {
         return [PackageServiceProvider::class];
@@ -16,14 +20,39 @@ class TestCase extends OrchestraTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        Route::get('login', ['as' => 'login', 'uses' => 'sinkcup\LaravelMakeAuthSocialite\Http\Controllers\SocialiteLoginController@showLoginForm']);
-        $this->loadLaravelMigrations();
+        Route::get('login', [
+            'uses' => 'App\Http\Controllers\Auth\LoginController@showLoginForm',
+            'as' => 'login',
+        ]);
+        Route::get('login/{provider}', [
+            'uses' => 'App\Http\Controllers\Auth\LoginController@redirectToProvider',
+        ]);
+        Route::get('login/{provider}/callback', [
+            'uses' => 'App\Http\Controllers\Auth\LoginController@handleProviderCallback',
+        ]);
+        Route::get('settings/profile', [
+            'uses' => 'App\Http\Controllers\ProfileController@edit',
+            'as' => 'profile.edit',
+        ]);
         @mkdir(__DIR__ . '/../vendor/orchestra/testbench-core/laravel/app/Http/Controllers/', 0755, true);
         @mkdir(__DIR__ . '/../vendor/orchestra/testbench-core/laravel/routes/');
+        @mkdir(__DIR__ . '/../vendor/orchestra/testbench-core/laravel/app/Http/Controllers/Auth/');
+        @mkdir(__DIR__ . '/../vendor/orchestra/testbench-core/laravel/tests/Feature/', 0755, true);
+        @unlink(__DIR__ . '/../vendor/orchestra/testbench-core/laravel/routes/web.php');
         touch(__DIR__ . '/../vendor/orchestra/testbench-core/laravel/routes/web.php');
+        copy(__DIR__ . '/User.stub', __DIR__ . '/../vendor/orchestra/testbench-core/laravel/app/User.php');
         $this->artisan('make:auth', ['--force' => true])->run();
+        $this->artisan('make:auth-socialite', ['--force' => true])->run();
+        $this->loadLaravelMigrations();
         $this->loadMigrationsFrom(__DIR__ . '/../src/database/migrations');
         $this->artisan('migrate')->run();
+        $this->app->register(\Laravel\Socialite\SocialiteServiceProvider::class);
+        $this->app->make('Illuminate\Contracts\Http\Kernel')
+            ->pushMiddleware('Illuminate\Session\Middleware\StartSession');
+        $client_id = $this->faker->md5;
+        $client_secret = $this->faker->sha1;
+        $redirect = $this->faker->url;
+        $this->serviceConfig = compact('client_id', 'client_secret', 'redirect');
     }
 
     /**
