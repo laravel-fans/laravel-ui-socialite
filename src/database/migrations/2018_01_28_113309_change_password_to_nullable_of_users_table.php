72fa50b391d985d1d32b06193c684a7d3b1c4671
sinkcup <sinkcup@gmail.com>
2019-09-07 11:40:08 +0800

    HACK: NOT NULL constraint failed: users.password #20

--- src/database/migrations/2018_01_28_113309_change_password_to_nullable_of_users_table.php
+++ src/database/migrations/2018_01_28_113309_change_password_to_nullable_of_users_table.php
@@ -26,7 +26,6 @@ class ChangePasswordToNullableOfUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('password')->nullable(false)->change();
         });
     }
 }
