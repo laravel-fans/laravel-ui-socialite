db6fcd2282dc0d5bd5629eed1e69d86273657517
sinkcup <sinkcup@gmail.com>
2020-08-30 19:42:00 +0800

    feat: support Laravel 7 #43

--- phpunit.sh
+++ phpunit.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 STARTED_AT=$(date +%s)
 
+rm -f vendor/orchestra/testbench-core/laravel/migrations/2014_10_12_100000_create_password_resets_table.php
 ./vendor/bin/phpunit --stop-on-defect --coverage-text tests/
 if [ $? -ne 0 ]; then
     exit 1
